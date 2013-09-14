7cc0787f425a98eb01309798c8820bd15586f4ad
Jonathan Pulfer <JonPulfer@server.fake>
2013-09-14 08:10:33 +0100

    Grabbed a copy of twofish.go and updated to work with current version of go.

---
+++ secure/secure.go
@@ -0,0 +1,10 @@
+/* Package secure provides encryption methods.
+
+These methods are used to handle encrypted streams such as the login process for
+the client.
+*/
+package secure
+
+import (
+	"fmt"
+)
